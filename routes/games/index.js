ce3b55cae0450171eb5d79a0caabec381b35a4da
Dexter <dtaylor13@live.com>
2025-07-04 13:41:39 -0400

    add show route to games

--- routes/games/index.js
+++ routes/games/index.js
@@ -1,10 +1,25 @@
 'use strict'
 
-
+const games = require('../../data/games');
 
 module.exports = async function (fastify, opts) {
-    const games = require('../../data/games');
+    // Index
   fastify.get('/', async function (request, reply) {
+
     return games
-  })
+  });
+
+    // Show
+  fastify.get('/:id', async function (request, reply) {
+
+    const {id} = request.params;
+
+    const game = games.find((game) => {
+        return game.id === id
+        
+    });
+
+    return game
+  });
+
 }
